1c4d6ba1684a645a8d18ab440e21dd81c169b861
Monit <monitbisht04@gmail.com>
2026-02-07 21:04:27 +0530

    Added fragments (home, search, forecast)

---
+++ Dewy/app/src/main/java/io/github/monitbisht/dewy/ForecastFragment.java
@@ -0,0 +1,23 @@
+package io.github.monitbisht.dewy;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ForecastFragment extends Fragment {
+
+    public ForecastFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_forecast, container, false);
+    }
+}
