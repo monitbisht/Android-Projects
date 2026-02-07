c3607f7f26a5f34d66eb4da6c9ac9a2bd4d7e323
Monit <monitbisht04@gmail.com>
2026-02-07 21:26:09 +0530

    Completed bottom navigation UI and fragment switching

--- Dewy/app/src/main/java/io/github/monitbisht/dewy/MainActivity.java
+++ Dewy/app/src/main/java/io/github/monitbisht/dewy/MainActivity.java
@@ -1,14 +1,31 @@
 package io.github.monitbisht.dewy;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainerView;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    FragmentContainerView fragmentContainer;
+
+    private final Fragment homeFragment = new HomeFragment();
+    private final Fragment searchFragment = new SearchFragment();
+    private final Fragment forecastFragment = new ForecastFragment();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +37,38 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        fragmentContainer = findViewById(R.id.fragment_container);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        // Switch fragments based on bottom tab selection
+       bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+           @Override
+           public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment selectedFragment ;
+
+                int itemId = menuItem.getItemId();
+
+               if (itemId == R.id.nav_home) {
+                   selectedFragment = homeFragment;
+               } else if (itemId == R.id.nav_search) {
+                   selectedFragment = searchFragment;
+               } else {
+                   selectedFragment = forecastFragment;
+               }
+
+               getSupportFragmentManager()
+                       .beginTransaction()
+                       .replace(R.id.fragment_container,selectedFragment)
+                       .commit();
+
+               return true;
+           }
+       });
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, homeFragment)
+                .commit();
+
     }
 }
