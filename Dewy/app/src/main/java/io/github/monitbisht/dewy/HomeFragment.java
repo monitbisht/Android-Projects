f27320331037c86f9d33b30249b4aee4d6d5176f
Monit <monitbisht04@gmail.com>
2026-02-12 00:19:07 +0530

    Implemented runtime location permission handling in HomeFragment

--- Dewy/app/src/main/java/io/github/monitbisht/dewy/HomeFragment.java
+++ Dewy/app/src/main/java/io/github/monitbisht/dewy/HomeFragment.java
@@ -1,9 +1,15 @@
 package io.github.monitbisht.dewy;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,14 +17,42 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class HomeFragment extends Fragment {
+    ActivityResultLauncher<String[]> locationPermissionRequest;
+
 
     public HomeFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        locationPermissionRequest = registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(),
+                result -> {
+
+                    Boolean fineLocationGranted = null;
+                    Boolean coarseLocationGranted = null;
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                        fineLocationGranted = result.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false);
+                        coarseLocationGranted = result.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION,false);
+                    }
+
+                    if ( (fineLocationGranted != null && fineLocationGranted )|| (coarseLocationGranted != null && coarseLocationGranted)) {
+                        fetchLocation();
+                    }
+                    else {
+                        Toast.makeText(getContext(), "Location Permission Denied" , Toast.LENGTH_LONG )
+                                .show();
+                    }
+                });
     }
 
 
@@ -27,13 +61,23 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
-
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         RecyclerView rv = view.findViewById(R.id.hourly_forecast_rv);
+
+        // Checking the location permission
+        if(isLocationPermissionGranted()){
+            fetchLocation();
+        }
+        // Else requesting it from the user
+        else {
+            requestPermission();
+        }
+
+
         rv.setLayoutManager(
                 new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false)
         );
@@ -51,6 +95,27 @@ public class HomeFragment extends Fragment {
 
         rv.setAdapter(new HourlyForecastAdapter(mockData));
 
-
     }
+
+    private boolean isLocationPermissionGranted() {
+        boolean fineGranted =  ContextCompat.checkSelfPermission(requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED ;
+
+        boolean coarseGranted = ContextCompat.checkSelfPermission(getContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+
+        return fineGranted || coarseGranted ;
+    }
+
+    public void requestPermission(){
+            locationPermissionRequest.launch(new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            });
+        }
+
+        private void fetchLocation(){
+            //TODO
+        }
 }
