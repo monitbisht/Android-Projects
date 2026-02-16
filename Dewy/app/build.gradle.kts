56322ced88519177a2868dc95e827403fe4702ee
Monit <monitbisht04@gmail.com>
2026-02-17 00:59:00 +0530

    Revert "Added Retrofit and Gson converter dependencies"
    This reverts commit a5b9598d9aec525d3bbf07ada1701221a790d048.

--- Dewy/app/build.gradle.kts
+++ Dewy/app/build.gradle.kts
@@ -44,10 +44,4 @@ dependencies {
 
     // Location dependency for Google Play services
     implementation(libs.google.play.services.location)
-
-    // Retrofit dependency
-    implementation(libs.retrofit)
-
-    // Retrofit GSON xconverter dependency
-    implementation(libs.converter.gson)
 }
